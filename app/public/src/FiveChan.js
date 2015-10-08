02ebdc8b293515d35a5188cf46326abca62bffd6
Aaron <aarongkendall@gmail.com>
2015-10-08 17:27:56 +0100

    callback added

--- app/public/src/FiveChan.js
+++ app/public/src/FiveChan.js
@@ -4,12 +4,14 @@ console.log('do the funk');
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST", "/json", true);
     xhttp.send(postCapture());
+    var response = postDisplay();
+    document.getElementById('test').innerHTML = response[0].post;
   };
 
   function postCapture() {
     var message = document.getElementById("message").value;
     return JSON.stringify({"post" : message});
-  }
+  };
 
   function postDisplay() {
     var xhttpGet = new XMLHttpRequest();
